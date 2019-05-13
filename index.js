79d6ed80b564781a3825d40d755c94d1e11a0881
Mathew Heard <splitice@users.noreply.github.com>
2019-05-13 17:18:25 +1000

    inital code commit

---
+++ index.js
@@ -0,0 +1,77 @@
+class QPromise extends Promise {
+	constructor(executor) {
+			super(executor);
+	}
+
+	timeout(ms, message){
+		const deferred = Q.deferred()
+		setTimeout(function(){
+			const e = new Error(message ? message : ("Timed out after " + ms + " ms"))
+			e.code = 'ETIMEDOUT'
+			deferred.reject(e)
+		}, ms)
+		this.then(function(r){
+			deferred.resolve(r)
+		})
+		return deferred.promise
+	}
+
+	delay(ms){
+		return this.then(function(){
+			return Q.delay(ms)
+		})
+	}
+}
+
+function Q(value){
+	if(value && value.then){
+		return QPromise.resolve().then(()=>value)
+	}else{
+		return QPromise.resolve(value)
+	}
+}
+
+Q.deferred = function(){
+	const d = {}
+	d.promise = Q(new QPromise((resolve, reject)=>{
+		d.resolve = resolve
+		d.reject = reject
+	}))
+	return d
+}
+
+Q.delay = function(ms){
+	const deferred = Q.deferred()
+	setTimeout(function(){
+		deferred.resolve()
+	}, ms)
+	return deferred.promise
+}
+
+Q.fcall = function(fn, ...args){
+	return new QPromise((resolve, reject)=>{
+		try {
+			resolve(fn(...args))
+		} catch(ex){
+			reject(ex)
+		}
+	})
+}
+
+Q.nfcall = function(fn,...args){
+	return new QPromise((resolve, reject)=>{
+		try {
+			fn(...args, function(err, result){
+				if(err) {
+					reject(err)
+					return
+				}
+				resolve(result)
+			})
+		} catch(ex){
+			reject(ex)
+		}
+	})
+}
+
+module.exports = Q
